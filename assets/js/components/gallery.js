087b74e0e2da29ba865b81d7507c1ea61fbcb813
Karlito8888 <charles.bourgault@hotmail.fr>
2024-09-10 14:30:09 +0200

    Réajustement de toutes les images du site (formats et tailles)

--- assets/js/components/gallery.js
+++ assets/js/components/gallery.js
@@ -28,83 +28,84 @@ const filters = [
 // Données pour les éléments de la galerie
 const galleryItems = [
   {
-    src: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash.jpg",
+    src: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-500w.jpg",
     srcsetWebp:
-      "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash.webp 500w, ./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-small.webp 250w",
+      "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-500w.webp 500w, ./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-250w.webp 250w",
     srcsetJpeg:
-      "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash.jpg 500w, ./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-small.jpg 250w",
+      "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-500w.jpg 500w, ./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash-250w.jpg 250w",
     tag: "Concert",
     alt: "Photo illustrant une foule assistant à un concert",
   },
   {
-    src: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash.jpg",
+    src: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-500w.jpg",
     srcsetWebp:
-      "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash.webp 500w, ./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-small.webp 250w",
+      "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-500w.webp 500w, ./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-250w.webp 250w",
     srcsetJpeg:
-      "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash.jpg 500w, ./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-small.jpg 250w",
+      "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-500w.jpg 500w, ./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash-250w.jpg 250w",
     tag: "Entreprises",
     alt: "Photo illustrant un homme d’affaires croisant les bras",
   },
   {
-    src: "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash.jpg",
+    src: "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-500w.jpg",
     srcsetWebp:
-      "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash.webp 500w, ./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-small.webp 250w",
+      "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-500w.webp 500w, ./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-250w.webp 250w",
     srcsetJpeg:
-      "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash.jpg 500w, ./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-small.jpg 250w",
+      "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-500w.jpg 500w, ./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash-250w.jpg 250w",
     tag: "Entreprises",
     alt: "Photo illustrant deux femmes et collègues qui sourient",
   },
   {
-    src: "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash.jpg",
+    src: "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-500w.jpg",
     srcsetWebp:
-      "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash.webp 500w, ./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-small.webp 250w",
+      "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-500w.webp 500w, ./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-250w.webp 250w",
     srcsetJpeg:
-      "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash.jpg 500w, ./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-small.jpg 250w",
+      "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-500w.jpg 500w, ./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash-250w.jpg 250w",
     tag: "Mariages",
     alt: "Photo illustrant les bras d’un homme et d’une femme qui se rejoignent",
   },
   {
-    src: "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash.jpg",
+    src: "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-500w.jpg",
     srcsetWebp:
-      "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash.webp 500w, ./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-small.webp 250w",
+      "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-500w.webp 500w, ./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-250w.webp 250w",
     srcsetJpeg:
-      "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash.jpg 500w, ./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-small.jpg 250w",
+      "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-500w.jpg 500w, ./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash-250w.jpg 250w",
     tag: "Portrait",
     alt: "Photo illustrant un homme fermant les yeux face au soleil",
   },
   {
-    src: "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash.jpg",
+    src: "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash-500w.jpg",
     srcsetWebp:
-      "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash.webp 500w, ./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash-small.webp 250w",
+      "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash-500w.webp 500w, ./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash-250w.webp 250w",
     srcsetJpeg:
-      "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash.jpg 500w, ./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash-small.jpg 250w",
+      "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash-500w.jpg 500w, ./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash-250w.jpg 250w",
     tag: "Mariages",
     alt: "Photo illustrant un jeune couple marié",
   },
   {
-    src: "./assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash.jpg",
+    src: "./assets/images/gallery/portraits/nino-van-prattenburg-443cl1uR_8-unsplash-500w.jpg",
     srcsetWebp:
-      "./assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash.webp 500w, ./assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash-small.webp 250w",
+      "./assets/images/gallery/portraits/nino-van-prattenburg-443cl1uR_8-unsplash-500w.webp 500w, ./assets/images/gallery/portraits/nino-van-prattenburg-443cl1uR_8-unsplash-250w.webp 250w",
     srcsetJpeg:
-      "./assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash.jpg 500w, ./assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash-small.jpg 250w",
+      "./assets/images/gallery/portraits/nino-van-prattenburg-443cl1uR_8-unsplash-500w.jpg 500w, ./assets/images/gallery/portraits/nino-van-prattenburg-443cl1uR_8-unsplash-250w.jpg 250w",
     tag: "Portrait",
     alt: "Photo illustrant une jeune femme posant derrière un grillage",
   },
+
   {
-    src: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash.jpg",
+    src: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-500w.jpg",
     srcsetWebp:
-      "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash.webp 500w, ./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-small.webp 250w",
+      "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-500w.webp 500w, ./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-250w.webp 250w",
     srcsetJpeg:
-      "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash.jpg 500w, ./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-small.jpg 250w",
+      "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-500w.jpg 500w, ./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash-250w.jpg 250w",
     tag: "Concert",
     alt: "Photo illustrant un chanteur sur une scène de concert",
   },
   {
-    src: "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash.jpg",
+    src: "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-500w.jpg",
     srcsetWebp:
-      "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash.webp 500w, ./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-small.webp 250w",
+      "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-500w.webp 500w, ./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-250w.webp 250w",
     srcsetJpeg:
-      "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash.jpg 500w, ./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-small.jpg 250w",
+      "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-500w.jpg 500w, ./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash-250w.jpg 250w",
     tag: "Entreprises",
     alt: "Photo illustrant une jeune femme qui sourit devant son ordinateur de bureau",
   },
@@ -187,25 +188,30 @@ export function generateGallery() {
   galleryItems.forEach((item) => {
     const picture = document.createElement("picture");
 
+    // Source WebP avec différentes résolutions
     const sourceWebp = document.createElement("source");
     sourceWebp.srcset = item.srcsetWebp;
     sourceWebp.type = "image/webp";
 
-    const sourceJpg = document.createElement("source");
-    sourceJpg.srcset = item.srcsetJpeg;
-    sourceJpg.type = "image/jpeg";
+    // Source JPEG avec différentes résolutions
+    const sourceJpeg = document.createElement("source");
+    sourceJpeg.srcset = item.srcsetJpeg;
+    sourceJpeg.type = "image/jpeg";
 
+    // Image par défaut
     const img = document.createElement("img");
     img.src = item.src;
     img.setAttribute("data-gallery-tag", item.tag);
     img.classList.add("gallery-item");
-    img.loading = "lazy";
+    img.loading = "lazy"; // Charger paresseux pour améliorer la performance
     img.alt = item.alt;
 
+    // Ajouter les sources et l'image au conteneur picture
     picture.appendChild(sourceWebp);
-    picture.appendChild(sourceJpg);
+    picture.appendChild(sourceJpeg);
     picture.appendChild(img);
 
+    // Ajouter le conteneur picture à la galerie
     galleryContainer.appendChild(picture);
   });
 
